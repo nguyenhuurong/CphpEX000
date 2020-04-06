5b94231d0084a02ef280da277924c11a04d3d5db
nguyenhuurong <51846617+nguyenhuurong@users.noreply.github.com>
2020-04-06 16:19:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>Cloud-Php-Ex000</title>
+  <title>CloudPhpEx000</title>
   <meta charset="utf-8">
 </head>
 <body>
